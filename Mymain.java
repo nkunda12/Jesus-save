909395a3dbf27d7f94580dd91b9523154a6196c3
nkunda12 <63205281+nkunda12@users.noreply.github.com>
2020-04-05 16:23:41 -0400

    very

---
+++ Mymain.java
@@ -0,0 +1,23 @@
+package myConstructor;
+
+public class Mymain {
+
+	public static void main(String[] args) {
+		
+		NoMain myObj = new NoMain(40);
+		
+		NoMain myObj2 = new NoMain("Boss");
+		
+		NoMain myObj3 = new NoMain(2);
+		
+		NoMain myObj4 = new NoMain("50",51);
+		
+		NoMain myObj5 = new NoMain(80,"81");
+		
+		NoMain myObj6 = new NoMain(90,"91");
+		
+	
+
+	}
+
+}
